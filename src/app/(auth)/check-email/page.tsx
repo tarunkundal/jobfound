7ce5b151cbf0d73d3a59d3bb8628d109dff40d36
Tarun Chauhan <chauhan271singh@gmail.com>
2025-12-08 13:23:20 +0530

    check email page props fixed

--- src/app/(auth)/check-email/page.tsx
+++ src/app/(auth)/check-email/page.tsx
@@ -2,17 +2,15 @@ import { ROUTES } from "@/constants/routes";
 import { Button } from "@/theme/ui/components/button";
 import Link from "next/link";
 
-interface PageProps {
-    params?: { [key: string]: string | string[] | undefined }; // For dynamic route segments
-    searchParams?: { [key: string]: string | string[] | undefined }; // For URL query parameters
-}
+export const runtime = "nodejs";
 
-const CheckEmailPage = async ({ searchParams }: PageProps) => {
+const CheckEmailPage = async ({ searchParams }: { searchParams: Promise<{ email?: string }> }) => {
+    const { email } = await searchParams;
     return (
         <div className="text-primary flex flex-col items-center justify-center bg-body w-[80]% h-1/2 mx-auto my-auto p-4 rounded-md shadow-md gap-6 m-2 shadow-card">
             <h1 className="text-6xl">Check your email</h1>
             <p className="text-secondary text-2xl text-center">
-                We sent a confirmation link to <b>{searchParams?.email}</b>. Please verify your account before logging in.
+                We sent a confirmation link to <b>{email}</b>. Please verify your account before logging in.
             </p>
             <Button variant="default" size="lg" className="mt-4">
                 <Link href={ROUTES.AUTH.LOGIN} prefetch={true}>Go to Login</Link>
