48f8fbdad01e6dd0dd49978d05785bd4557ad2fe
Tarun Chauhan <chauhan271singh@gmail.com>
2025-12-08 18:29:11 +0530

    default theme fixes

--- src/app/providers/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider.tsx
@@ -9,7 +9,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return (
         <NextThemesProvider
             attribute="class"       // Apply theme as a CSS class (e.g., <html class="dark">)
-            defaultTheme="light"    // Default theme if none is set (or use "system")
+            defaultTheme="dark"    // Default theme if none is set (or use "system")
             enableSystem           // Enable detection of OS theme preference
             {...props}
         >
